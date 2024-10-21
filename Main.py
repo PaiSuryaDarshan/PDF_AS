c45741fb1d2778f4a9334717756e9b153863fbe5
PaiSuryaDarshan <paisuryadarshan.research@gmail.com>
2024-10-21 17:57:16 +0100

    update 2 files and create 1 file

--- Main.py
+++ Main.py
@@ -1,28 +1,44 @@
+import sys
+import os
 import Annotation_marking_Segregate as AM_S
 
-pdf_path = "D:\iCloudDrive\Desktop\Academics\Year_3\Lab\SC Papers\(Onder_Peckan) Effect of temperature on Gelation properties.pdf"
-# pdf_path = "C:/Users/surya/Downloads/p1_(Song_Jian).pdf"
+if sys.platform == 'darwin':
+    LIT_REV_DIR = "/Users/pai.suryadarshan/Desktop/Academics/Year_3/(3000 word) Lit. Rev"
+if sys.platform == 'win32':
+    LIT_REV_DIR = "D:\iCloudDrive\Desktop\Academics\\Desktop\Academics\Year_3\(3000 word) Lit. Rev"
 
-list_of_extracts = AM_S.complete_function(pdf_path)
+SORTED_DIR = sorted(os.listdir(LIT_REV_DIR))
 
-MOTIVATION_Yel  = list_of_extracts[0] 
-METHODS_Grn     = list_of_extracts[1] 
-CONTEXT_Blu     = list_of_extracts[2] 
-RESULTS_Pur     = list_of_extracts[3] 
+for index, filename in enumerate(SORTED_DIR):
+    options = str(index) + " ====> " + filename
+    print(options)
+
+print(os.listdir(LIT_REV_DIR))
+
+# papername = ""
+
+# pdf_path = LIT_REV_DIR + '\ '.strip() + papername
+
+# list_of_extracts = AM_S.complete_function(pdf_path)
+
+# MOTIVATION_Yel  = list_of_extracts[0] 
+# METHODS_Grn     = list_of_extracts[1] 
+# CONTEXT_Blu     = list_of_extracts[2] 
+# RESULTS_Pur     = list_of_extracts[3] 
 # UNKNOWN_FUNC_Pin= list_of_extracts[4] 
-FUTURE_red      = list_of_extracts[5]   
-INTERPRETATION_Ora= list_of_extracts[6] 
-TITLE_Bla       = list_of_extracts[7] 
+# FUTURE_red      = list_of_extracts[5]   
+# INTERPRETATION_Ora= list_of_extracts[6] 
+# TITLE_Bla       = list_of_extracts[7] 
 # UNKNOWN_FUNC_Gra= list_of_extracts[8] 
 # UNKNOWN_FUNC_LBl= list_of_extracts[9] 
 
-AM_S.Segregate(".\Extracts\_0_TITLE.txt",   TITLE_Bla)                    
-AM_S.Segregate(".\Extracts\_1_MOTIVATION.txt", MOTIVATION_Yel)                    
-AM_S.Segregate(".\Extracts\_2_METHODS.txt", METHODS_Grn)                    
-AM_S.Segregate(".\Extracts\_3_CONTEXT.txt", CONTEXT_Blu)                    
-AM_S.Segregate(".\Extracts\_4_RESULTS.txt", RESULTS_Pur)                    
-AM_S.Segregate(".\Extracts\_5_INTERPRETATION.txt", INTERPRETATION_Ora)                    
-AM_S.Segregate(".\Extracts\_6_FUTURE.txt", FUTURE_red)                    
+# AM_S.Segregate(".\Extracts\_0_TITLE.txt",   TITLE_Bla)                    
+# AM_S.Segregate(".\Extracts\_1_MOTIVATION.txt", MOTIVATION_Yel)                    
+# AM_S.Segregate(".\Extracts\_2_METHODS.txt", METHODS_Grn)                    
+# AM_S.Segregate(".\Extracts\_3_CONTEXT.txt", CONTEXT_Blu)                    
+# AM_S.Segregate(".\Extracts\_4_RESULTS.txt", RESULTS_Pur)                    
+# AM_S.Segregate(".\Extracts\_5_INTERPRETATION.txt", INTERPRETATION_Ora)                    
+# AM_S.Segregate(".\Extracts\_6_FUTURE.txt", FUTURE_red)                    
 
 # ----------------------------------------------------------------
 #* REFERENCE FOR DEVELOPER
